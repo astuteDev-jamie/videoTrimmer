966ab244f91e8b44e5e8c5d51ed1b028d9f4093c
Onwuasoanya James <onwuasoanyajames@gmail.com>
2022-12-28 15:53:55 +0100

    pointer correction

--- videoTrimmer.js
+++ videoTrimmer.js
@@ -15,9 +15,12 @@ const trackButton = document.getElementById('tracker')
 const trimLeftBtn = document.querySelector('div.left')
 const trimRightBtn = document.querySelector('div.right')
 const trimBtn = document.getElementById('trim-button')
+const LtrimText = document.getElementById('.left span')
+const RtrimText = document.getElementById('.right span')
+const slider = document.querySelector('.strip')
+const thumb = document.querySelector('.thumb')
 
-
-// trimLeftBtn.addEventListener('mousedown',)
+// trimLeftBtn.addEventListener('mousedown touchstart',)
 videoinput.addEventListener('change',e=>{
     // h3.innerText=''
     CTA.style.display='none'
@@ -41,54 +44,117 @@ videoinput.addEventListener('change',e=>{
     }
 })
 
-totalTimeline.addEventListener('mousemove', updateTimeline)
-totalTimeline.addEventListener('mousedown', changeScrubbing)
-document.addEventListener('mouseup', e=> {
-    let sqr = totalTimeline.getBoundingClientRect()
-    const percentage = Math.min(Math.max(0, e.x -sqr.x),sqr.width)/sqr.width
-    updateTimeline(e)
-    if (scrubbing) {
-    scrubbing = !scrubbing
-        if (video.paused) video.play()
-        playPauseButton.classList.toggle('paused')
-        video.currentTime = percentage * video.duration
-    }
-    if (lBound){
-        lBound= !lBound
-        
-    }
-    if (rBound){
-        rBound = !rBound
-    }
-})
+// totalTimeline.addEventListener('pointermove', updateTimeline)
+totalTimeline.addEventListener('pointerdown', changeScrubbing)
+// document.addEventListener('pointereup', e=> {
+//     updateTimeline(e)
+//     let sqr = totalTimeline.getBoundingClientRect()
+//     const percentage = Math.min(Math.max(0, e.x -sqr.x),sqr.width)/sqr.width
+//     let rightSqr = trimRightBtn.getBoundingClientRect()
+//     let leftSqr = trimLeftBtn.getBoundingClientRect()
+//     let leftWidth = `${rightSqr.x - leftSqr.x}px`
+//     trimLeftBtn.style.setProperty('--dist',leftWidth)
 
-document.addEventListener('mousemove', e=> {
-    if (scrubbing || lBound || rBound) updateTimeline(e)
-})
+//     if (scrubbing) {
+//     scrubbing = !scrubbing
+//         if (video.paused) video.play()
+//         playPauseButton.classList.toggle('paused')
+//         video.currentTime = percentage * video.duration
+//     }
+//     if (lBound){
+//         lBound= !lBound
+//     }
+//     if (rBound){
+//         rBound = !rBound
+//     }
+// })
+
+// document.addEventListener('pointermove', e=> {
+//     e.preventDefault()
+//     if (scrubbing || lBound || rBound) updateTimeline(e)
+
+// })
 
 let lBound =false
 let rBound =false
 let scrubbing = false
+
+
 function changeScrubbing(e){
     let sqr = totalTimeline.getBoundingClientRect()
     const percentage = Math.min(Math.max(0, e.x -sqr.x),sqr.width)/sqr.width
-    if (e.buttons===1 & !trackButton.hasAttribute('hidden')){
-        scrubbing = !scrubbing
-        if (scrubbing){
-            if (!playPauseButton.classList.contains('paused')){
-                video.pause()
-                playPauseButton.classList.toggle('paused')
-            } 
-        } 
+   
+    if  (e.target == trackButton & !trackButton.hasAttribute('hidden')){
+        trackButton.setPointerCapture(e.pointerId);
         video.currentTime = percentage * video.duration
-        // updateTimeline(e)
-    } 
-    if (e.buttons===1 & e.target==trimLeftBtn){
-        lBound=!lBound
+            if (!playPauseButton.classList.contains('paused')){
+                 video.pause()
+                 playPauseButton.classList.toggle('paused')
+             } 
+
+    }else if(e.target==trimLeftBtn & !trimLeftBtn.hasAttribute('hidden')){
+        console.log('left')
+        trimLeftBtn.setPointerCapture(e.pointerId);
+
+    }else if(e.target==trimRightBtn & !trimRightBtn.hasAttribute('hidden')){
+        console.log('right')
+        trimRightBtn.setPointerCapture(e.pointerId);
     }
-    if (e.buttons===1 & e.target==trimRightBtn){
-        rBound=!rBound
-    }
+  
+    totalTimeline.onpointermove = function(e) {
+        let sqr = totalTimeline.getBoundingClientRect()
+        let rightSqr = trimRightBtn.getBoundingClientRect()
+        let leftSqr = trimLeftBtn.getBoundingClientRect()
+        const percentage = Math.min(Math.max(0, e.x -sqr.x),sqr.width)/sqr.width
+        const leftWidth = `${rightSqr.x - leftSqr.x}px`
+        currentTimeline.style.setProperty('--preview',percentage)
+
+        if (e.target == trackButton){
+            trackButton.style.setProperty('--currentPosition',percentage)
+            currentTimeline.style.setProperty('--currentPos',percentage)
+        }
+
+        if (e.target ==  trimLeftBtn){
+            console.log(e.pointerId)
+            trimLeftBtn.style.setProperty('--bound',percentage)
+            trimLeftBtn.style.setProperty('--dist',leftWidth)
+        }
+
+        if (e.target == trimRightBtn){
+            console.log('moving')
+            trimRightBtn.style.setProperty('--bound',percentage)
+            trimLeftBtn.style.setProperty('--dist',leftWidth)
+        }
+    };
+  
+    totalTimeline.onpointerup = function(e) {
+        let sqr = totalTimeline.getBoundingClientRect()
+        const percentage = Math.min(Math.max(0, e.x -sqr.x),sqr.width)/sqr.width
+        let rightSqr = trimRightBtn.getBoundingClientRect()
+        let leftSqr = trimLeftBtn.getBoundingClientRect()
+        let leftWidth = `${rightSqr.x - leftSqr.x}px`
+        trimLeftBtn.style.setProperty('--dist',leftWidth)
+
+
+        if  (e.target == trackButton & !trackButton.hasAttribute('hidden')){
+            if (video.paused) {video.play()
+                playPauseButton.classList.toggle('paused')
+                video.currentTime = percentage * video.duration
+            }
+        }
+
+        if(e.target==trimLeftBtn & !trimLeftBtn.hasAttribute('hidden')){
+            console.log('left out')
+
+        }
+
+        if(e.target==trimRightBtn & !trimRightBtn.hasAttribute('hidden')){
+            console.log('right out')
+        
+        }
+      totalTimeline.onpointermove = null;
+      trackButton.onpointerup = null;
+    };
 }
 
 function updateTimeline(e){
@@ -100,14 +166,13 @@ function updateTimeline(e){
    currentTimeline.style.setProperty('--preview',percentage)
 
    if (scrubbing){
-    e.preventDefault()
     currentTimeline.style.setProperty('--currentPos',percentage)
     trackButton.style.setProperty('--currentPosition',percentage)
    }
 
    if (lBound ){
-    trimLeftBtn.style.setProperty('--bound',percentage)
-    trimLeftBtn.style.setProperty('--dist',leftWidth)
+       trimLeftBtn.style.setProperty('--bound',percentage)
+       trimLeftBtn.style.setProperty('--dist',leftWidth)
    }
    if (rBound ){
     trimRightBtn.style.setProperty('--bound',percentage)
@@ -128,6 +193,7 @@ playPauseButton.addEventListener('click',e=>{
 
 trimBtn.addEventListener('click', e=>{
     if (document.querySelector('.control-container').classList.contains('active')){
+        console.log('smelling')
     trimRightBtn.style.setProperty('--bound',1)
     currentTimeline.classList.toggle('timeline')
     trackButton.toggleAttribute('hidden')
@@ -139,7 +205,9 @@ trimBtn.addEventListener('click', e=>{
     trimLeftBtn.style.setProperty('--dist',leftWidth)}
 })
 
-function resetTotaltime(){}
+function setLRtrim(){
+
+}
 
 function logTime(value){
     const format = new Intl.NumberFormat(undefined, {minimumIntegerDigits:2})
@@ -151,3 +219,19 @@ function logTime(value){
     else {return `${hours}:${format.format(minutes)}:${format.format(seconds)}`}
 }
 
+thumb.onpointerdown = function(e) {
+    console.log(e.pointerId)
+    let sqr = slider.getBoundingClientRect()
+    thumb.setPointerCapture(e.pointerId);
+    
+    thumb.onpointermove = function(e) {
+        console.log(e.pointerId)
+      let newLeft = Math.min(Math.max(0, e.x -sqr.x),sqr.width)/sqr.width
+      thumb.style.setProperty('--currentPosition',newLeft)
+    };
+  
+    thumb.onpointerup = function(e) {
+      thumb.onpointermove = null;
+      thumb.onpointerup = null;
+    };
+  };
